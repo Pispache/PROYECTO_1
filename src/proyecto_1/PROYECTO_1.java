65d0adc900c75774602b532f5fec86fda4f7703e
CesarTecun <ctecunl1@miumg.edu.gt>
2024-03-13 00:02:25 -0600

    Initial commit

---
+++ src/proyecto_1/PROYECTO_1.java
@@ -0,0 +1,16 @@
+package proyecto_1;
+
+/**
+ *
+ * @author cesar
+ */
+public class PROYECTO_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
